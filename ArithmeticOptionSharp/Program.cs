aada81babff5abd19c6cf6193e343fd951ae7730
Evangelina <greengreyblack80@gmail.com>
2020-07-05 23:27:53 +0300

    first commit

---
+++ ArithmeticOptionSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ArithmeticOptionSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(^\-?\d+\.?\d*[\+\-\*\/\%]{1}\d+\.?\d*)|(^\-?\d+[\+\-]{2}$)|(^[\+\-]{2}\d+$)");
+            Console.WriteLine("Введите выражение:");
+            string str = Console.ReadLine();
+            str = str.Replace(" ", "");
+            if (regex.IsMatch(str))
+            {
+                Console.WriteLine("Выражение соответствует синтаксису языка");
+            }
+            else Console.WriteLine("Выражение не соответствует синтаксису языка");
+            Console.ReadKey();
+        }
+    }
+}
